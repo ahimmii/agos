646bccca14f0737be7a5b4f2907bd3a520e9537a
Oussama ZOUAINE <oussamazouaine@gmail.com>
2023-03-17 10:59:33 +0100

    feat: add media query

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger-menu');
+const items = document.querySelectorAll('.accordion button');
 
 console.log(menu, burger);
 
@@ -7,3 +8,17 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('open');
     burger.classList.toggle('open');
 });
+
+function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
