6dad53ed7599338bfe295b578938fca1b61973c6
Oussama ZOUAINE <oussamazouaine@gmail.com>
2023-03-17 17:58:51 +0100

    finishing the pages

--- js/main.js
+++ js/main.js
@@ -22,3 +22,35 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+// JQuery
+$(document).ready(function () {
+    // link action
+    $('.action').click(function (e) {
+        e.preventDefault();
+
+        $('.slide').removeClass('active');
+        var slide = $(this).closest('.slide');
+        slide.addClass('active');
+    });
+});
+
+// checkWidth
+checkWidth = function () {
+    var windowsize = $(window).width();
+    if (windowsize > 480) {
+        var slideWidth = $('.active').width();
+        $('.slide-content').css({
+            width: slideWidth + 'px',
+        });
+    }
+};
+
+$(window).resize(function () {
+    // onresize
+    checkWidth();
+
+    // finish resize
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout(checkWidth, 500);
+});
