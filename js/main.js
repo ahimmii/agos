0b546464f19bc72eb929c4ef59006d5c1a307465
Oussama ZOUAINE <oussamazouaine@gmail.com>
2023-03-27 10:49:24 +0000

    fix: fix responsiveness for index page

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger-menu');
 const items = document.querySelectorAll('.accordion button');
 
-console.log(menu, burger);
-
 burger.addEventListener('click', () => {
     menu.classList.toggle('open');
     burger.classList.toggle('open');
