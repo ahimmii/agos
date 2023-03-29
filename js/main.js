a5c04edd7873c4b793f29a81cec829ac21479411
Oussama ZOUAINE <oussamazouaine@gmail.com>
2023-03-29 06:50:29 +0000

    fix: make sector list clickable

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
+const sectors = [
+    {
+        id: '01',
+        name: 'healthcare',
+        desc: 'Healthcare facilities represent unique security challenges, which is why they require special and specific services.',
+    },
+    {
+        id: '02',
+        name: 'shopping centers and store',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptate.',
+    },
+    {
+        id: '03',
+        name: 'residential highrise buildings and compounds',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptate.',
+    },
+    {
+        id: '04',
+        name: 'industrial and manufacturing plants',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptate.',
+    },
+    {
+        id: '05',
+        name: 'events and venues',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptate.',
+    },
+];
+
 const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger-menu');
 const items = document.querySelectorAll('.accordion button');
+
+const sectorsOfActivity = document.querySelectorAll('#sectors-of-activity');
+const sectorsDesc = document.querySelector('.sectors .desc');
+
+sectorsOfActivity.forEach((sector) => {
+    sector.addEventListener('click', (e) => {
+        const filteredSector = sectors.filter(
+            (sectorObject) =>
+                sectorObject.name === sector.innerText.toLowerCase()
+        );
+
+        sectorsDesc.getElementsByTagName('h3')[0].innerHTML =
+            filteredSector[0].id;
+
+        sectorsDesc.getElementsByTagName('p')[0].innerHTML =
+            filteredSector[0].desc;
+    });
+});
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('open');
